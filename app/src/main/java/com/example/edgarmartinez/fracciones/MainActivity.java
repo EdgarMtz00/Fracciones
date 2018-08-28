7c569b4b03df06c0978fd1857db60d24ce205b34
Edgar Martinez <agustin__mtz00@outlook.com>
2018-08-27 22:13:08 -0500

    Black magic was done here

--- app/src/main/java/com/example/edgarmartinez/fracciones/MainActivity.java
+++ app/src/main/java/com/example/edgarmartinez/fracciones/MainActivity.java
@@ -11,10 +11,12 @@ import static java.lang.Integer.parseInt;
 public class MainActivity extends AppCompatActivity {
     public EditText Num1, Num2, NumR, Den1, Den2, DenR;
     public Button Suma, Resta, Multiplicacion, Division;
+    public int num, den;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Num1 = (EditText)findViewById(R.id.etNum1);
         Num2 = (EditText)findViewById(R.id.etNum2);
         NumR = (EditText)findViewById(R.id.etNumR);
@@ -25,101 +27,38 @@ public class MainActivity extends AppCompatActivity {
         Resta = (Button) findViewById(R.id.btnResta);
         Division = (Button) findViewById(R.id.btnDivision);
         Multiplicacion = (Button) findViewById(R.id.btnMultiplicacion);
-
     }
     public void sumaClick(View p){
-
-        String texto = Num1.getText().toString();
-        int num1 = parseInt(texto);
-
-        texto = Den1.getText().toString();
-        int den1 = parseInt(texto);
-
-        texto = Num2.getText().toString();
-        int num2 = parseInt(texto);
-
-        texto = Den2.getText().toString();
-        int den2 = parseInt(texto);
-        if( den1 == 0 || den2 == 0){
-            NumR.setText("ERROR");
-            DenR.setText("ERROR");
-        }else {
-            NumR.setText("" + ((num1 * den2) + (num2 * den1)));
-            DenR.setText("" + (den1 * den2));
+        fraccion res = new fraccion(parseInt(Num1.getText().toString()),parseInt(Den1.getText().toString())).sumar(new fraccion(parseInt(Num2.getText().toString()),parseInt(Den2.getText().toString())));
+        NumR.setText("" + res.num);
+        if(res.num!=0){
+            DenR.setText("" + res.den);
         }
     }
 
     public void restaClick(View p){
-        String texto = Num1.getText().toString();
-        int num1 = parseInt(texto);
-
-        texto = Den1.getText().toString();
-        int den1 = parseInt(texto);
-
-        texto = Num2.getText().toString();
-        int num2 = parseInt(texto);
-
-        texto = Den2.getText().toString();
-        int den2 = parseInt(texto);
-        if( den1 == 0 || den2 == 0){
-            NumR.setText("ERROR");
-            DenR.setText("ERROR");
-        }else {
-            NumR.setText("" + ((num1 * den2) - (num2 * den1)));
-            DenR.setText("" + (den1 * den2));
+        fraccion res =new fraccion(parseInt(Num1.getText().toString()),parseInt(Den1.getText().toString())).restar(new fraccion(parseInt(Num2.getText().toString()),parseInt(Den2.getText().toString())));
+        NumR.setText("" + res.num);
+        if(res.num!=0){
+            DenR.setText("" + res.den);
         }
     }
 
     public void multClick(View p){
-        String texto = Num1.getText().toString();
-        int num1 = parseInt(texto);
-
-        texto = Den1.getText().toString();
-        int den1 = parseInt(texto);
-
-        texto = Num2.getText().toString();
-        int num2 = parseInt(texto);
-
-        texto = Den2.getText().toString();
-        int den2 = parseInt(texto);
-        if( den1 == 0 || den2 == 0){
-            NumR.setText("ERROR");
-            DenR.setText("ERROR");
-        }else {
-            NumR.setText("" + (num1 * num2));
-            DenR.setText("" + (den1 * den2));
+        fraccion res =new fraccion(parseInt(Num1.getText().toString()),parseInt(Den1.getText().toString())).multiplicar(new fraccion(parseInt(Num2.getText().toString()),parseInt(Den2.getText().toString())));
+        NumR.setText("" + res.num);
+        if(res.num!=0){
+            DenR.setText("" + res.den);
         }
     }
 
     public void divClick(View p){
-        String texto = Num1.getText().toString();
-        int num1 = parseInt(texto);
-
-        texto = Den1.getText().toString();
-        int den1 = parseInt(texto);
-
-        texto = Num2.getText().toString();
-        int num2 = parseInt(texto);
-
-        texto = Den2.getText().toString();
-        int den2 = parseInt(texto);
-        if( den1 == 0 || num2 == 0 || den2 == 0){
-            NumR.setText("ERROR");
-            DenR.setText("ERROR");
-        }else {
-            NumR.setText("" + (num1 * den2));
-            DenR.setText("" + (den1 * num2));
+        fraccion res =new fraccion(parseInt(Num1.getText().toString()),parseInt(Den1.getText().toString())).division(new fraccion(parseInt(Num2.getText().toString()),parseInt(Den2.getText().toString())));
+        NumR.setText("" + res.num);
+        if(res.num!=0){
+        DenR.setText("" + res.den);
         }
     }
 
-    public void conversion(int num, int den){
-        int menor = (num<den)?num:den;
-        for(int i=0;i == menor;i++){
-            if(num % i == 0 && den % i ==0){
-                conversion(num/i,den/i);
-                return;
-            }
-        }
 
-    }
 }
--- app/src/main/java/com/example/edgarmartinez/fracciones/fraccion.java
+++ app/src/main/java/com/example/edgarmartinez/fracciones/fraccion.java
@@ -1,7 +1,7 @@
 package com.example.edgarmartinez.fracciones;
 
 public class fraccion {
-    int den, num;
+    public int den, num;
 
     fraccion(){
 
@@ -14,25 +14,37 @@ public class fraccion {
 
     public fraccion sumar(fraccion sumando){
         if(this.den==sumando.den){
-            return new fraccion(this.num+sumando.num,this.den);
+            return conversion(new fraccion(this.num+sumando.num,this.den));
         }else{
-            return new fraccion((this.num*sumando.den)+(this.den*sumando.num),this.den*sumando.den);
+            return conversion(new fraccion((this.num*sumando.den)+(this.den*sumando.num),this.den*sumando.den));
         }
     }
 
     public fraccion restar(fraccion restando){
         if(this.den==restando.den){
-            return new fraccion(this.num-restando.num,this.den);
+            return conversion(new fraccion(this.num-restando.num,this.den));
         }else{
-            return new fraccion((this.num*restando.den)-(this.den*restando.num),this.den*restando.den);
+            return conversion(new fraccion((this.num*restando.den)-(this.den*restando.num),this.den*restando.den));
         }
     }
 
     public fraccion multiplicar(fraccion multiplicador){
-        return new fraccion(this.num*multiplicador.num,this.den*multiplicador.den);
+        return conversion(new fraccion(this.num*multiplicador.num,this.den*multiplicador.den));
     }
 
     public fraccion division(fraccion divisor){
-        return new fraccion(this.num*divisor.den,this.den*divisor.num);
+        return conversion(new fraccion(this.num*divisor.den,this.den*divisor.num));
+    }
+
+    private fraccion conversion(fraccion f){
+        int menor = (f.num<f.den)?f.num:f.den;
+        for(int i=2;i <= menor;i++){
+            if(f.num % i == 0 && f.den % i ==0){
+                fraccion res = conversion(new fraccion(f.num/i, f.den/i));
+                return res;
+            }
+        }
+        return f;
+
     }
 }
